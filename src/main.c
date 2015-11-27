90f2e27a8619acbe2d6dadd06539c4cb4a8a0922
Daniel Friesel <derf@finalrewind.org>
2015-11-28 00:07:57 +0100

    working i2cdetect \o/

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <msp430.h>
+#include <string.h>
 
 volatile char prompt[64];
 volatile unsigned int prompt_pos = 0;
@@ -60,6 +61,67 @@ void uart_setup(void)
 	UCA0IE |= UCRXIE;
 }
 
+int i2c_setup()
+{
+	UCB0CTL1 = UCSWRST;
+	UCB0CTLW0 = UCMODE_3 | UCMST | UCSYNC | UCSSEL_2 | UCSWRST | UCCLTO_1;
+	UCB0BRW = 0xf00;
+	P1DIR &= ~(BIT6 | BIT7);
+	P1SEL0 &= ~(BIT6 | BIT7);
+	P1SEL1 |= BIT6 | BIT7;
+
+	// use internal pull-ups
+	P1OUT |= (BIT6 | BIT7);
+	P1REN |= (BIT6 | BIT7);
+
+	UCB0CTL1 &= ~UCSWRST;
+	UCB0I2CSA = 0;
+
+	if (UCB0STAT & UCBBUSY)
+		return -1;
+	return 0;
+}
+
+void i2c_scan()
+{
+	int slave_addr;
+
+	uart_puts("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f");
+	for (slave_addr = 0; slave_addr < 128; slave_addr++) {
+
+		if ((slave_addr & 0x0f) == 0) {
+			uart_putchar('\n');
+			uart_puthex(slave_addr);
+			uart_putchar(':');
+		}
+
+		UCB0I2CSA = slave_addr;
+		UCB0CTL1 |= UCTR | UCTXSTT | UCTXSTP;
+
+		while (UCB0CTL1 & UCTXSTP);
+
+		if (UCB0IFG & UCNACKIFG) {
+			uart_puts(" --");
+			UCB0IFG &= ~UCNACKIFG;
+		} else {
+			uart_putchar(' ');
+			uart_puthex(slave_addr);
+		}
+	}
+	uart_puts("\n");
+}
+
+void check_command()
+{
+	if (!strcmp(prompt, "i2cdetect")) {
+		if (i2c_setup() < 0) {
+			uart_puts("Error initalizing I²C: Bus is busy\n");
+			return;
+		}
+		i2c_scan();
+	}
+}
+
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;
@@ -103,13 +165,23 @@ __interrupt void USCI_A0_ISR(void)
 		buf = UCA0RXBUF;
 		if (buf == '\r') {
 
+			uart_putchar('\n');
+			check_command();
 			prompt_pos = 0;
-			uart_puts("\nmsp430fr5969 > ");
+			*prompt = 0;
+			uart_puts("msp430fr5969 > ");
 
 		} else if (buf == '\f') {
 
 			uart_puts("\nmsp430fr5969 > ");
 			uart_nputs(prompt, prompt_pos);
+
+		} else if (buf == 0x7f) {
+
+			if (prompt_pos) {
+				prompt_pos--;
+				uart_puts("\e[D \e[D");
+			}
 
 		} else if (buf >= ' ') {
 
